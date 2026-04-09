af44dc806bc8dd3c31fcf92babad7fed4fb6e22e
Kamron76-web <boltaboyevkamron70@gmail.com>
2026-04-09 21:15:34 +0500

    google verify

--- payway/urls.py
+++ payway/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 from core import views
+
+
+def google_verify(request):
+    return HttpResponse("google-site-verification: googlecd5090acaf63a595.html")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +18,5 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('job/<int:job_id>/', views.job_detail, name='job_detail'),
     path('job/<int:job_id>/bid/', views.add_bid, name='add_bid'),
+    path('googlecd5090acaf63a595.html', google_verify),
 ]
